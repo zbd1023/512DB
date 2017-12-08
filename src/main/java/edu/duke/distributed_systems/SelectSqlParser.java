e0bc7b69de9c21b5b74da068dae4a54c0cd0e82b
Wilson <bodong.zhang96@gmail.com>
2017-12-07 21:28:51 -0500

    added select parser

--- src/main/java/edu/duke/distributed_systems/SelectSqlParser.java
+++ src/main/java/edu/duke/distributed_systems/SelectSqlParser.java
@@ -1,17 +1,46 @@
 package edu.duke.distributed_systems;
+import net.sf.jsqlparser.parser.*;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
+
+import net.sf.jsqlparser.statement.*;
+import net.sf.jsqlparser.statement.select.*;
+import net.sf.jsqlparser.util.*;
+import java.util.*;
+import java.util.List;
 
 public class SelectSqlParser {
     //support 3 types of query: SELECT, INSERT, CREATE TABLE
-    String query;
-    public SelectSqlParser(String q){
-        this.query = q;
+    PlainSelect p;
+    public SelectSqlParser(String q) throws Exception{
+        Select select  = (Select) CCJSqlParserUtil.parse(q);
+        this.p = (PlainSelect)select.getSelectBody();
     }
     // SELECT a,v,c FROM asdf WHERE a = 1;
-    public String[] getColumns(){
-        int start = 7;
-        int end = this.query.indexOf("FROM");
-        return this.query.substring(start, end).split(",");
+    public List<String> getColumns(){
+        List<String> res = new ArrayList<>();
+        List<SelectItem> sl  = p.getSelectItems();
+        if(sl.get(0) instanceof AllColumns){
+            res.add("*");
+        }
+        else{
+            this.p.getSelectItems().forEach(a ->res.add(((Column)(((SelectExpressionItem )a).getExpression())).getColumnName()));
+        }
+        return res;
     }
+    public String getTable(){
+        return ((Table)(this.p.getFromItem())).getName();
+    }
+    public String getWhere(){
+        if(this.p.getWhere() != null){
+            List<String> res = new ArrayList<>();
+            return this.p.getWhere().toString();
+        }
+        else
+            return "";
+
+    }
+
 
 
 
--- src/main/java/edu/duke/distributed_systems/Start.java
+++ src/main/java/edu/duke/distributed_systems/Start.java
@@ -13,6 +13,10 @@ public class Start {
             for(int i =0; i < KVStores.length; i++){
                 KVStores[i] = system.actorOf(KVStore.props(), "KVStore" + i);
             }
+            SelectSqlParser s = new SelectSqlParser("select a,b,c from asdf where a>c or b=c");
+            System.out.println(s.getColumns());
+            System.out.println(s.getTable());
+            System.out.println(s.getWhere());
             System.out.println(">>> Press ENTER to exit <<<");
             System.in.read();
         } catch (IOException ioe) {
