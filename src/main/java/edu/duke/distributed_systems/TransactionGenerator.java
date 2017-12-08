6489c294410d68d7b77494ee83ddb77033e505b8
Daniel Chai <dhc10@duke.edu>
2017-12-08 02:29:49 -0500

    Implement basic version of TransactionGenerator (previously SqlLayer)

---
+++ src/main/java/edu/duke/distributed_systems/TransactionGenerator.java
@@ -0,0 +1,82 @@
+package edu.duke.distributed_systems;
+
+import java.util.*;
+
+public class TransactionGenerator {
+	private MetadataStore metadataStore;
+	
+	public TransactionGenerator(MetadataStore metadataStore) {
+		this.metadataStore = metadataStore;
+	}
+	
+	/**
+	 * Generates a Transaction (list of ScanActions) from a SELECT statement
+	 * 
+	 * WHERE clause is currently ignored... the problem is the WHERE clause 
+	 * can only be used after the transaction is exectuted (after which all
+	 * the rows have been retrieved). This means the WHERE filtering is 
+	 * outside the scope of this method.
+	 */
+	public Transaction makeSelectTransaction(String query) throws Exception {
+		SelectSqlParser parser = new SelectSqlParser(query);
+		
+		String table = parser.getTable();
+		List<String> columns = parser.getColumns();
+		String where = parser.getWhere();
+		
+		String firstPrimaryKey = metadataStore.getFirstPrimaryKey(table);
+		String lastPrimaryKey = metadataStore.getLastPrimaryKey(table);
+		
+		// create a ScanAction for each column to be selected
+		List<Action> actionList = new ArrayList<Action>();
+		for (int i = 0; i < columns.size(); i++) {
+			String startKey = table + "/" + firstPrimaryKey + "/" + columns.get(i);
+			String endKey = table + "/" + lastPrimaryKey + "/" + columns.get(i);
+			actionList.add(new ScanAction(startKey, endKey));
+		}
+		
+		Transaction tx = new Transaction(actionList);
+		return tx;
+	}
+	
+	/**
+	 * Generates a Transaction (list of PutActions) from an INSERT statement
+	 */
+	public Transaction makeInsertTransaction(String query) throws Exception {
+		InsertSqlParser parser = new InsertSqlParser(query);
+		
+		String table = parser.getTable();
+		List<String> columns = parser.getColumns();
+		List<String> values = parser.getValues();
+		
+		String primaryKey = metadataStore.getPrimaryKey(table);
+		String primaryKeyValue = "";
+		
+		// find the primary key value in the inserted row
+		for (int i = 0; i < columns.size(); i++) {
+			if (columns.get(i).equals(primaryKey)) {
+				primaryKeyValue = values.get(i);
+				break;
+			}
+		}
+		
+		// update table metadata (first and last primary key) if necessary
+		if (primaryKeyValue.compareTo(metadataStore.getFirstPrimaryKey(table)) < 0) {
+			metadataStore.setFirstPrimaryKey(table, primaryKeyValue);
+		}
+		if (primaryKeyValue.compareTo(metadataStore.getLastPrimaryKey(table)) > 0) {
+			metadataStore.setLastPrimaryKey(table, primaryKeyValue);
+		}
+		
+		// create a PutAction for each column in the inserted row
+		List<Action> actionList = new ArrayList<Action>();
+		for (int i = 0; i < columns.size(); i++) {
+			String key = table + "/" + primaryKeyValue + "/" + columns.get(i);
+			String value = values.get(i);
+			actionList.add(new PutAction(key, value));
+		}
+		
+		Transaction tx = new Transaction(actionList);
+		return tx;
+	}
+}
